6aad33e7b055d530ffab1f73b1ef3d3184ec42ea
AkumuYuma <manumanu1355@gmail.com>
2021-04-25 17:24:33 +0200

    aggiornamento

--- Session1/mazeGenerator/cell.js
+++ Session1/mazeGenerator/cell.js
@@ -1,19 +1,71 @@
-
-
 class Cell {
   constructor(x, y) {
     // Posizione
-    this.x = x * scale;
-    this.y = y * scale;
-    this.walls = [true, true, true, true] // Top, right, bottom left walls
+    this.x = x;
+    this.y = y;
+    // Muri
+    this.walls = [true, true, true, true] // Top, right, bottom, left walls
+    // Visitata
+    this.visited = false;
+  }
+
+  visit() {
+    // Visita la cella
+    this.visited = true;
   }
 
   show() {
+    // Disegna la cella con i suoi muri. Di un colore diverso se visitata
     noFill();
     stroke(255);
-    if (this.walls[0]) line(this.x, this.y, this.x + scale, this.y);
-    if (this.walls[1]) line(this.x + scale, this.y, this.x + scale, this.y + scale);
-    if (this.walls[2]) line(this.x + scale, this.y + scale, this.x, this.y + scale);
-    if (this.walls[3]) line(this.x, this.y + scale, this.x, this.y);
+    let x = this.x * scale;
+    let y = this.y * scale;
+    if (this.walls[0]) line(x, y, x + scale, y);
+    if (this.walls[1]) line(x + scale, y, x + scale, y + scale);
+    if (this.walls[2]) line(x + scale, y + scale, x, y + scale);
+    if (this.walls[3]) line(x, y + scale, x, y);
+    // Se la cella è visitata la contrassegno di un colore diverso
+    if (this.visited) {
+      noStroke();
+      fill(136, 193, 215);
+      rect(x, y, scale, scale);
+    }
   }
+
+  vicini() {
+    // Resituisce un array con i vicini disponibili
+    let vicini = [];
+
+    try {
+      // Cerco di prendere il vicino di sopra
+      // Nota che se l'indice x va fuori dal range sollevo un errore. Lo gestisco e non faccio niente
+      // Se invece l'indice y va fuori dal range, semplicemente top sarà undefined. Questo secondo caso viene gestito dall'if top
+      let top = griglia[this.x][this.y-1];
+      if (top && !top.visited) vicini.push(top);
+    } catch {}
+    try {
+      let right = griglia[this.x + 1][this.y];
+      if (right && !right.visited) vicini.push(right);
+    } catch {}
+    try {
+      let bottom = griglia[this.x][this.y + 1];
+      if (bottom && !bottom.visited) vicini.push(bottom);
+    } catch {}
+    try {
+      let left = griglia[this.x - 1][this.y];
+      if (left && !left.visited) vicini.push(left);
+    } catch {}
+    finally {
+      return vicini;
+    }
+  }
+
+  getVicino() {
+    // Restituisce, se possibile, un vicino a caso
+    let vicini = this.vicini();
+    if (vicini.length > 0) {
+      return vicini[floor(random(0, vicini.length))];
+    }
+  }
+
 }
--- Session1/mazeGenerator/sketch.js
+++ Session1/mazeGenerator/sketch.js
@@ -1,21 +1,55 @@
-let celle = [];
+let griglia = []; // Array bidimensionale di celle
 let cols, rows;
 let scale = 20;
+let currentCell;
 
 
 function setup() {
   createCanvas(400, 400);
   background(0);
-  cols = floor(width/scale); // Numero di colonne
-  rows = floor(height/scale); // Numero di righe
+  cols = floor(width / scale); // Numero di colonne
+  rows = floor(height / scale); // Numero di righe
+  console.log("colonne: ", cols);
+  console.log("righe: ", rows);
 
-  for (let x = 0; x < rows; x++) {
-    for (let y = 0; y < cols; y++) {
-      celle.push(new Cell(x, y));
-    }
+  createGrid();
+  // Inizio dalla prima cella in alto a destra
+  currentCell = griglia[0][0];
+
+}
+
+function draw() {
+  // Implemento l'algoritmo direttamente qui utilizzando il loop predefinito di draw di p5js 
+  frameRate(10);
+
+  // Imposto visitata la cella corrente
+  currentCell.visit();
+  // Prendo la prossima cella
+  let next = currentCell.getVicino();
+  // Se si può prendere una cella, aggiorno la cella corrente
+  if (next) {
+    currentCell = next;
   }
+  // Disegno la griglia
+  showGrid();
 
-  for (cella of celle) {
-    cella.show(); 
+}
+
+
+function showGrid() {
+  // Disegna la griglia
+  for (let row of griglia) {
+    for (let cella of row) cella.show();
+  }
+}
+
+function createGrid() {
+  // Popolo l'array di celle
+  for (let x = 0; x < rows; x++) {
+    let row = []
+    for (let y = 0; y < cols; y++) {
+      row.push(new Cell(x, y));
+    }
+    griglia.push(row);
   }
 }
